3d30595bae701e83342cf60ecb1ab88f7dc894d6
marwendot <marwendot@gmail.com>
2020-02-08 10:10:26 -0800

    Buttons added to Second Activity.

--- app/src/main/java/videodemos/example/systemshack2020/JobMenu.java
+++ app/src/main/java/videodemos/example/systemshack2020/JobMenu.java
@@ -3,6 +3,7 @@ package videodemos.example.systemshack2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class JobMenu extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class JobMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_menu);
+
+        Toast.makeText(this," Job Postings", Toast.LENGTH_LONG).show();
     }
 }
