2ce4aa8dbd8c888cf76040cbc97194a831394238
marwendot <marwendot@gmail.com>
2020-02-08 10:37:39 -0800

    Created AppliedToActivity and ApplicationActivity.

--- app/src/main/java/videodemos/example/systemshack2020/JobMenu.java
+++ app/src/main/java/videodemos/example/systemshack2020/JobMenu.java
@@ -1,9 +1,15 @@
 package videodemos.example.systemshack2020;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Toast;
+import videodemos.example.systemshack2020.MenuActivity.ApplicationActivity;
+import videodemos.example.systemshack2020.MenuActivity.AppliedToPostings;
 
 public class JobMenu extends AppCompatActivity {
 
@@ -12,6 +18,24 @@ public class JobMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_menu);
 
-        Toast.makeText(this," Job Postings", Toast.LENGTH_LONG).show();
+        Button appliedPostings = (Button) findViewById(R.id.btnAppliedTo);
+        appliedPostings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent applied_to = new Intent(JobMenu.this, AppliedToPostings.class);
+                startActivity(applied_to);
+            }
+        });
+
+        Button dueDate = (Button) findViewById(R.id.btnDueDate);
+        dueDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent due_date_activity = new Intent(JobMenu.this, ApplicationActivity.class);
+                startActivity(due_date_activity);
+            }
+        });
+
+        Toast.makeText(this, " Job Postings", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/videodemos/example/systemshack2020/MenuActivity/ApplicationActivity.java
@@ -0,0 +1,16 @@
+package videodemos.example.systemshack2020.MenuActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import videodemos.example.systemshack2020.R;
+
+public class ApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application);
+    }
+}
