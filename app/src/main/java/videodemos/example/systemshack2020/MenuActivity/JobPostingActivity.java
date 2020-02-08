70362a6db8d42754c19bc0da04cd932eae9e9adc
Karen Chen <karencyuan@gmail.com>
2020-02-08 10:21:30 -0800

    Add new activity

---
+++ app/src/main/java/videodemos/example/systemshack2020/MenuActivity/JobPostingActivity.java
@@ -0,0 +1,15 @@
+package videodemos.example.systemshack2020.MenuActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import videodemos.example.systemshack2020.R;
+
+import android.os.Bundle;
+
+public class JobPostingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_posting);
+    }
+}
